30156c02bfa9b4488728b6f474c48f2f494c6795
AniketDhanai <96654182+AniketDhanai@users.noreply.github.com>
2024-12-19 08:21:15 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "testsubnet" {
-        vpc_id = aws_vpc.myvpc.id
-        cidr_block = "10.88.1.0/24"
+resource "aws_subnet" "mysubnet" {
+	vpc_id = aws_vpc.myvpc.id
+	cidr_block = "10.0.1.0/24"
 }
